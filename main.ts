038ca929a2f43d557169a77ebba10c57a4bab6d0
MrReseburg <94501244+MrReseburg@users.noreply.github.com>
2021-11-16 17:52:51 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ input.onButtonPressed(Button.A, function () {
     music.playMelody("C5 G B A F A C5 B ", 200)
     music.playMelody("C D E F G A B C5 ", 200)
 })
+basic.pause(100)
 basic.forever(function () {
     basic.showLeds(`
         # . . . #
